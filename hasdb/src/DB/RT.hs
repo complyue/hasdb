d67807232962a8cb75c8021c9696e93e49a810b5
Compl Yue <compl.yue@icloud.com>
2020-03-01 01:50:09 +0800

    wip

--- hasdb/src/DB/RT.hs
+++ hasdb/src/DB/RT.hs
@@ -6,6 +6,8 @@ import           Prelude
 
 import           Control.Monad.Reader
 
+import           Control.Concurrent.STM
+
 import qualified Data.HashMap.Strict           as Map
 
 import           Language.Edh.EHI
@@ -30,22 +32,28 @@ classNameProc !argsSender !exit = do
             (EdhArgsPack $ ArgsPack argsCls $ Map.map classNameOf kwargs)
  where
   classNameOf :: EdhValue -> EdhValue
-  classNameOf (EdhClass (ProcDefi _ (ProcDecl _ cn _ _))) = EdhString cn
+  classNameOf (EdhClass (ProcDefi _ (ProcDecl _ !cn _ _))) = EdhString cn
+  classNameOf (EdhObject !obj) =
+    EdhString $ procedure'name $ procedure'decl $ objClass obj
   classNameOf _ = nil
 
 
--- | utility newBo(*args,**kwargs)
+-- | utility newBo(boClass, sbEnt)
 newBoProc :: EdhProcedure
 newBoProc !argsSender !exit =
   packHostProcArgs argsSender $ \(ArgsPack !args !kwargs) -> case args of
-    [EdhClass !cls] | Map.null kwargs ->
+    [EdhClass !cls, EdhObject !sbEnt] | Map.null kwargs ->
       createEdhObject cls $ \(OriginalValue boVal _ _) -> case boVal of
         EdhObject !bo -> do
           pgs <- ask
           let world = contextWorld $ edh'context pgs
           contEdhSTM $ do
             boScope <- mkScopeWrapper world $ objectScope bo
-            exitEdhSTM pgs exit $ EdhTuple [EdhObject bo, EdhObject boScope]
+            modifyTVar' (objSupers bo) (sbEnt :)
+            modifyTVar' (entity'store $ objEntity sbEnt)
+              $ Map.insert (AttrByName "_boScope")
+              $ EdhObject boScope
+            exitEdhSTM pgs exit $ EdhObject bo
         _ -> error "bug: createEdhObject returned non-object"
-    _ -> throwEdh EvalError "should pass the class as the only arg to `newBo`"
+    _ -> throwEdh EvalError "Invalid arg to `newBo`"
 
